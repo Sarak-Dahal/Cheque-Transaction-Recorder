bdefe4694d93e9fee9551bd4616f027aeb10ef0a
Sarak <dahal9sarak@gmail.com>
2025-03-15 15:59:40 -0600

    Test

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Sanodad Store Web App</h1>
+      <p>Welcome to the Sanodad Store!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'; // Import your CSS file
+import App from './App'; // Import your main App component (create this file next)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
